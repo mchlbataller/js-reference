9cdeff6b5a2a8b07a442f7c26ac894b5105c8e66
Michael Bataller <mchlbataller@gmail.com>
2020-03-24 04:05:51 +0800

    added for loop

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript Reference
+Author: Michael Bataller
+
+## Table of Contents
+1. For-loops in JavaScript
---
+++ for.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5,5,1,2,3,1,2,4];
+
+for(let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    /* Outputs
+    1
+    2
+    3
+    4
+    5
+    5
+    1
+    2
+    3
+    1
+    2
+    4
+    */
+}
