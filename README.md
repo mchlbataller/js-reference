9c7259eb55439032f275f100a1f3cfc4dd00bffb
Michael Bataller <mchlbataller@gmail.com>
2020-03-24 04:07:36 +0800

    added improved for-loop

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Author: Michael Bataller
 
 ## Table of Contents
-1. For-loops in JavaScript
+1. For-loops
+2. IMPROVED For-loop
---
+++ improved-for.js
@@ -0,0 +1,20 @@
+let arr = [1,2,3,4,5,5,1,2,3,1,2,4];
+
+for(let entry in arr) {
+    console.log(entry);
+    /* Outputs
+    1
+    2
+    3
+    4
+    5
+    5
+    1
+    2
+    3
+    1
+    2
+    4
+    */
+
+}
